70ac53cdb9aa2b37aa0609d668b6b580ab2bfbe9
Aminul-97 <amin147570bd@gmail.com>
2024-02-17 22:02:45 +0600

    test update

--- tests/test_leap_year.py
+++ tests/test_leap_year.py
@@ -2,5 +2,8 @@ from src.leap_year import check_leap_year
 import pytest
 
 
-def test_database():
+def test_leap_year():
     assert check_leap_year(2000) == "Leap Year"
+
+def test_not_leap_year():
+    assert check_leap_year(2001) == "Not Leap Year"
