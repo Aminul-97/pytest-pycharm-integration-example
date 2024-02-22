4e14644690ee6d108e11e0efeca5222a67d1e1ec
Aminul-97 <amin147570bd@gmail.com>
2024-02-22 22:59:49 +0600

    Doc Strings

--- src/leap_year.py
+++ src/leap_year.py
@@ -1,5 +1,14 @@
 
 def check_leap_year(year:int) -> str:
+    """
+    Function to check if the given year is a leap year.
+    
+    Args:
+    year: integer
+
+    Returns:
+    String "Leap Year" or "Not Leap Year"
+    """
     if (year % 400 == 0) and (year % 100 == 0):
         return "Leap Year"
     elif (year % 4 == 0) and (year % 100 != 0):
