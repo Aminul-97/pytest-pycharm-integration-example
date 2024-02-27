cce257228c5a41b44adef5d7c697d6271c1caa7e
Aminul-97 <amin147570bd@gmail.com>
2024-02-28 02:28:42 +0600

    Updated Docstring

--- src/leap_year.py
+++ src/leap_year.py
@@ -9,10 +9,18 @@ def check_leap_year(year:int) -> str:
     Returns:
     String "Leap Year" or "Not Leap Year"
     """
+
+    ## Divided by 100 means century year.
+    ## And century year divided by 400 means
     if (year % 400 == 0) and (year % 100 == 0):
         return "Leap Year"
+
+    ## Not divided by 100 means not a century year.
+    ## Year divided by 4 is a leap year.
     elif (year % 4 == 0) and (year % 100 != 0):
         return "Leap Year"
+
+    ## Not divided by 4 and 400 means not a leap year.
     else:
         return "Not Leap Year"
     
